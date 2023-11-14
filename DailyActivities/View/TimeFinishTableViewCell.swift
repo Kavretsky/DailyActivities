5e5a717a77cd112172b504b68ea352ba6e453b31
Nikolay Kavretsky <kavretsky@gmail.com>
2023-11-14 23:16:20 +0400

    stable activity edit form

---
+++ DailyActivities/View/TimeFinishTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeFinishTableViewCell.swift
+//  DailyActivities
+//
+//  Created by Nikolay Kavretsky on 14.11.2023.
+//
+
+import UIKit
+
+class TimeFinishTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DailyActivities/View/TimeStartTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TimeStartTableViewCell.swift
+//  DailyActivities
+//
+//  Created by Nikolay Kavretsky on 14.11.2023.
+//
+
+import UIKit
+
+class TimeStartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
