ae9fd724887685f6aa17cd254955bfb9624b8904
Nikolay Kavretsky <Kavretsky@gmail.com>
2023-11-24 19:37:39 +0300

    scroll to added activity

--- DailyActivities/View/MainViewController.swift
+++ DailyActivities/View/MainViewController.swift
@@ -107,11 +107,11 @@ extension MainViewController: NewActivityViewDelegate {
     func addNewActivity(description: String, typeID: String) {
         activityStore.addActivity(description: description, typeID: typeID)
         let indexPath = IndexPath(item: activityStore.activities(for: activityListDate).count - 1, section: 0)
-        print(indexPath)
         activitiesTableView.beginUpdates()
         activitiesTableView.reloadRows(at: [IndexPath(row: indexPath.row - 1, section: indexPath.section)], with: .none)
         activitiesTableView.insertRows(at: [indexPath], with: .top)
         activitiesTableView.endUpdates()
+        activitiesTableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
     }
     
     func showTypeManager() {
